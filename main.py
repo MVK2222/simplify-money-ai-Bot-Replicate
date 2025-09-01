e1c47cc948b3f510afcf26ed114c946d06296bf6
Bandela Mohan Venkata Krishna <141541049+MVK2222@users.noreply.github.com>
2025-09-01 08:34:08 +0530

    Update Uvicorn server port to 8080

--- main.py
+++ main.py
@@ -9,4 +9,4 @@ app = create_app()
 
 if __name__ == "__main__":
     logging.info("Starting Uvicorn server...")
-    run("main:app", host="127.0.0.1", port=10000, reload=True)
+    run("main:app", host="127.0.0.1", port=8080, reload=True)
